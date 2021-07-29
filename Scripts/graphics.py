93f22d11f2628b631a6e34d636ae0cbef1b62440
raylennon <48305386+raylennon@users.noreply.github.com>
2021-07-29 00:26:42 -0400

    syntax fix

--- Scripts/graphics.py
+++ Scripts/graphics.py
@@ -18,7 +18,7 @@ def frame(pos, dir, images):
         pp = player.crop((4,0,7,5))
     frame.paste(pp, (30,17), pp.convert('RGBA'))
     print(pos)
-    if not sb.getpixel(pos) == (0, 0, 0):
+    if not sb.getpixel(pos[0], pos[1]) == (0, 0, 0):
         frame.paste(overlay, (0,0), overlay)
 
     return frame.convert('RGB')
